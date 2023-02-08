1d700686beb2a052aa2b382d7f7a7f6c8b82c895
Tobias Mayer <tobi.mayer1990@gmail.com>
2023-02-08 19:30:28 +0100

    output scalar values

--- autograd.cpp
+++ autograd.cpp
@@ -23,7 +23,7 @@ private:
 public:
     Scalar(double data) : _data(data), _grad(0.0f), _backward{[]() {}} { }
 
-    Scalar(double data, vector<ValuePtr> children)
+    Scalar(double data, vector<ScalarPtr> children)
         : _data(data),
           _grad(0.0f),
           _children{children},
@@ -33,7 +33,11 @@ public:
     double data() const { return _data; }
     void set_data(double data) { _data = data; }
     double grad() const { return _grad; }
+    const vector<ScalarPtr>& children() const { return _children; }
     void set_grad(double grad) { _grad = grad; }
-    const vector<ValuePtr>& children() const { return _children; }
 
+    friend ostream& operator<<(ostream& os, const Scalar& scalar) {
+        os << "Scalar(data=" << scalar._data << ", grad=" << scalar.grad << ")";
+        return os;
+    }
 };
