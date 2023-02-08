539d2128d3efab72601b7dccf29a8870779e6704
Tobias Mayer <tobi.mayer1990@gmail.com>
2023-02-08 19:26:23 +0100

    add basic scalar definition

---
+++ autograd.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <memory>
+#include <ostream>
+#include <vector>
+
+using std::make_shared;
+using std::ostream;
+using std::shared_ptr;
+using std::vector;
+
+struct Scalar;
+using ScalarPtr = shared_ptr<Scalar>;
+
+struct Scalar : public std::enable_shared_from_this<Scalar> {
+private:
+    float _data;
+    double _grad;
+    vector<ScalarPtr> _children;
+    std::function<void()> _backward;
+
+public:
+    Scalar(double data) : _data(data), _grad(0.0f), _backward{[]() {}} { }
+
+    Scalar(double data, vector<ValuePtr> children)
+        : _data(data),
+          _grad(0.0f),
+          _children{children},
+          _backward{[]() {}} { }
+
+
+    double data() const { return _data; }
+    void set_data(double data) { _data = data; }
+    double grad() const { return _grad; }
+    void set_grad(double grad) { _grad = grad; }
+    const vector<ValuePtr>& children() const { return _children; }
+
+};
